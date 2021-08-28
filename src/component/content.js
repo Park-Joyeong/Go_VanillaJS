631c5d98b3e78cc09e3d35a614680a37eb99ed17
Park Joyeong <drhamst@gmail.com>
2021-08-29 00:08:40 +0900

    End of Repo

--- src/component/content.js
+++ src/component/content.js
@@ -1,11 +1,11 @@
-import { Component } from "../../core/component.js";
+import { Component } from "../core/component.js";
 
 export default class Content extends Component {
 
   createTasks(item) {
     return `
       <li>
-        <a href=${item.href}>
+        <a href="${item.href}">
           <img src=${item.imageUrl} width="100" height"200" style="display:inline;">
           <span>${item.title}</span>
         </a>
